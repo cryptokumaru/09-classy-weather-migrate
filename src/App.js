f3cb5abb5f73f206b9d896ceeb6f860ada6f7e18
Kumar MM <cryptokumaru@gmail.com>
2023-09-30 11:58:55 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [location, setLocation] = useState("Lisbon");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Classy Weather</h1>
+      <Input location={location} />
     </div>
   );
 }
 
-export default App;
+function Input({ location }) {
+  return (
+    <div>
+      <input type="text" placeholder="enter location" value={location}></input>
+    </div>
+  );
+}
